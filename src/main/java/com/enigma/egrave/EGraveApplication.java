aa1cc85d1d245359984c5f7228601a773a978502
Bambang <bambangkapr23@gmail.com>
2021-12-28 09:55:37 +0700

    Initial Commit

---
+++ src/main/java/com/enigma/egrave/EGraveApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.egrave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGraveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/egrave/EGraveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.egrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
